caeb51a0658a08ae72fa9f4df8d5f0a33635a4b5
Jerry-Banga <jerrybangabiau9@gmail.com>
2024-10-27 15:22:46 +0000

    reconfigured nativewind

--- app/auth/Welcome.jsx
+++ app/auth/Welcome.jsx
@@ -3,35 +3,18 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import CustomButton from '../../components/CustomButton'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native';
+import  "../../global.css"
 
 export default function Welcome() {
-  const navigation = useNavigation();
-  return (
-    <SafeAreaView className="py-6 px-4 h-full flex-1 ">
-        <View className="w-[100vw] h-[100vh] bg-primary-100 opacity-[0.55]">
-         </View>
-        <ScrollView className=" "
-            contentContainerStyle={{
-            height: "100%",}}>
-            <View className=" w-full my-auto flex justify-between items-center h-[80%] py-6 px-4">
-                <View className=" my-6">
-                    <Text className="font-bold text-center">
-                        Logo
-                    </Text>
-                </View>
-                <View className="my-">
-                    <Text className="font-bold text-center">
-                    Catch Phrase
-                    </Text>
-                </View>
-                <CustomButton
-                title="Commencer"
-                handlePress={() => navigation.navigate('Signup')}
-                containerStyles="w-full font-bold rounded-full bg-primary-200"/>
+    const navigation = useNavigation();
+    return (
+        <SafeAreaView className="h-full flex-1">
+            <View className="px-3">
+                <Text className="text-red-600">Logo</Text>
+                <View className="border border-cyan-900 h-9 w-9 bg-cyan-400"></View>
             </View>
-        </ScrollView>
-    </SafeAreaView>
-  )
+        </SafeAreaView>
+    )
 }
 
 const styles = StyleSheet.create({})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./<custom-folder>/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {
       colors: {
@@ -9,7 +10,7 @@ module.exports = {
           actionHover: "#1E456C",
         },
         primary:{
-          DEFAULT: "E6E3DC",
+          DEFAULT: "#E6E3DC",
         },
         secondary: {
           normal: "#29282E",
